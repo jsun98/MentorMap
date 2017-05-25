77779a0d84841ee2361066910425e5e648fc0c8e
Josh Sun <joshuasun1998@icloud.com>
2017-05-25 00:38:08 -0400

    cleanups

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+    path = require('path');
+    favicon = require('serve-favicon');
+    logger = require('morgan');
+    cookieParser = require('cookie-parser');
+    bodyParser = require('body-parser');
 
+//import route handlers
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -20,6 +21,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//serve statics
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
