b019e2d94fa326ea058f48ca4972b62d062516a3
Josh Sun <joshuasun1998@icloud.com>
2017-05-28 18:32:03 -0400

    integrated HTML5 boilerplate

--- app.js
+++ app.js
@@ -5,28 +5,27 @@ var express = require('express');
     cookieParser = require('cookie-parser');
     bodyParser = require('body-parser');
 
-//import route handlers
-var index = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//serve statics
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+//setup middleware
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-//serve statics
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
+//route handlers
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -43,7 +42,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('404');
 });
 
 module.exports = app;
