ca84a0c4f78e5da99c5a7a99a54edd55de33dc8a
Josh Sun <joshuasun1998@icloud.com>
2017-06-10 20:22:14 -0400

    converted to scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,19 @@
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
-var sass = require('gulp-ruby-sass');
+var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var livereload = require('gulp-livereload');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
 var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
 
 gulp.task('styles', function() {
-  return sass('public/css/scss/*.scss', { style: 'expanded' })
-    .pipe(autoprefixer('last 2 version'))
-    .pipe(gulp.dest('public/css'))
-    .pipe(livereload());
+    gulp.src('public/sass/*.scss')
+        .pipe(sass({outputStyle: 'expanded'})
+        .on('error', sass.logError))
+        .pipe(gulp.dest('./public/css'));
 });
 
 gulp.task('ejs',function(){
@@ -24,9 +25,11 @@ gulp.task('build', function () {
     return browserify({entries: 'react/main.js', extensions: ['.js'], debug: true})
         .transform(babelify, {presets: ["es2015", "react"]})
         .bundle()
+        .on("error", function (err) { console.log("Error : " + err.message); })
         .pipe(source('bundle.js'))
         .pipe(gulp.dest('public/js'));
 });
+
 
 gulp.task('scripts', function() {
   return gulp.src('react/**/*.js')
@@ -34,6 +37,7 @@ gulp.task('scripts', function() {
     .pipe(jshint.reporter('default'))
     .pipe(livereload());
 });
+
 
 gulp.task('eslint', function() {
   return gulp.src('react/main.js')
@@ -50,7 +54,7 @@ gulp.task('eslint', function() {
 
 gulp.task('watch', function() {
     livereload.listen();
-    gulp.watch('public/css/**/*.scss', ['styles']);
+    gulp.watch('public/sass/*.scss', ['styles']);
     //gulp.watch('react/**/*.js', ['scripts']);
     gulp.watch('views/**/*.ejs', ['ejs']);
     gulp.watch('react/**/*.js', ['build']);
