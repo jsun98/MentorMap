4fd7a887447fae46a74ee60bff362de6f6d145f1
Josh Sun <joshuasun1998@icloud.com>
2017-08-06 17:26:25 -0400

    incorporated calender onto front page

--- routes/mentee.js
+++ routes/mentee.js
@@ -6,38 +6,18 @@ router.use('*', isLoggedIn, isEmailVerified, isRegCompleted)
 
 router.get('/dashboard', (req, res, next) => {
 	User.findById(req.user._id)
-		.populate('upcomingSessions')
-		.populate({
-			path: 'upcomingSessions',
-			populate: {
-				path: 'mentor',
-				model: 'User',
-			},
-		})
-		.populate({
-			path: 'upcomingSessions',
-			populate: {
-				path: 'mentee',
-				model: 'User',
-			},
-		})
 		.sort({ 'upcomingSessions.date': -1 })
 		.exec((err, user) => {
 			if (err)
 				next(err)
 			user.upcomingSessions.sort((a, b) => new Date(a.date) - new Date(b.date))
-			if (req.user.role === 'mentee')
-				res.render('mentee_dashboard', {
-					user,
-					successMsg: req.flash('successMsg')[0] || '',
-					infoMsg: req.flash('infoMsg')[0] || '',
-				})
-			else
-				res.render('mentor_dashboard', {
-					user,
-					successMsg: req.flash('successMsg')[0] || '',
-					infoMsg: req.flash('infoMsg')[0] || '',
-				})
+			res.render('mentee_dashboard', { user })
+
+			// res.render('mentor_dashboard', {
+			// 	user,
+			// 	successMsg: req.flash('successMsg')[0] || '',
+			// 	infoMsg: req.flash('infoMsg')[0] || '',
+			// })
 
 		})
 
@@ -53,21 +33,14 @@ function isEmailVerified (req, res, next) {
 
 // checks if registration is completed
 function isRegCompleted (req, res, next) {
-	// if user is authenticated in the session, carry on
 	if (req.user.completed)
 		return next()
-
-	// if they aren't redirect them to the home page
-	res.redirect('/register')
+	res.redirect('/mentee/register')
 }
 
 function isLoggedIn (req, res, next) {
-	// if user is authenticated in the session, carry on
 	if (req.isAuthenticated())
 		return next()
-
-
-	// if they aren't redirect them to the home page
 	res.redirect('/auth/login')
 }
 
